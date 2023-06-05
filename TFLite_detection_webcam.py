60f1395105a1b435c3f4236c02441c13329b60db
Clementine Allen <tomtinderbundle@gmail.com>
2023-06-04 22:22:56 -0700

    format docs

--- TFLite_detection_webcam.py
+++ TFLite_detection_webcam.py
@@ -2,7 +2,7 @@
 #
 # Author: Evan Juras
 # Date: 10/27/19
-# Description: 
+# Description:
 # This program uses a TensorFlow Lite model to perform object detection on a live webcam
 # feed. It draws boxes and scores around the objects of interest in each frame from the
 # webcam. To improve FPS, the webcam object runs in a separate thread from the main program.
@@ -23,44 +23,44 @@ import importlib.util
 # Define VideoStream class to handle streaming of video from webcam in separate processing thread
 # Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
 # class VideoStream:
-    # """Camera object that controls video streaming from the Picamera"""
-    # def __init__(self,resolution=(640,480),framerate=30):
-    #     # Initialize the PiCamera and the camera image stream
-    #     self.stream = cv2.VideoCapture(0)
-    #     ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-    #     ret = self.stream.set(3,resolution[0])
-    #     ret = self.stream.set(4,resolution[1])
-            
-    #     # Read first frame from the stream
-    #     (self.grabbed, self.frame) = self.stream.read()
+# """Camera object that controls video streaming from the Picamera"""
+# def __init__(self,resolution=(640,480),framerate=30):
+#     # Initialize the PiCamera and the camera image stream
+#     self.stream = cv2.VideoCapture(0)
+#     ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+#     ret = self.stream.set(3,resolution[0])
+#     ret = self.stream.set(4,resolution[1])
 
-	# # Variable to control when the camera is stopped
-    #     self.stopped = False
+#     # Read first frame from the stream
+#     (self.grabbed, self.frame) = self.stream.read()
 
-    # def start(self):
-	# # Start the thread that reads frames from the video stream
-    #     Thread(target=self.update,args=()).start()
-    #     return self
+# # Variable to control when the camera is stopped
+#     self.stopped = False
 
-    # def update(self):
-    #     # Keep looping indefinitely until the thread is stopped
-    #     while True:
-    #         # If the camera is stopped, stop the thread
-    #         if self.stopped:
-    #             # Close camera resources
-    #             self.stream.release()
-    #             return
+# def start(self):
+# # Start the thread that reads frames from the video stream
+#     Thread(target=self.update,args=()).start()
+#     return self
 
-    #         # Otherwise, grab the next frame from the stream
-    #         (self.grabbed, self.frame) = self.stream.read()
+# def update(self):
+#     # Keep looping indefinitely until the thread is stopped
+#     while True:
+#         # If the camera is stopped, stop the thread
+#         if self.stopped:
+#             # Close camera resources
+#             self.stream.release()
+#             return
 
-    # def read(self):
-	# # Return the most recent frame
-    #     return self.frame
+#         # Otherwise, grab the next frame from the stream
+#         (self.grabbed, self.frame) = self.stream.read()
 
-    # def stop(self):
-	# # Indicate that the camera and thread should be stopped
-    #     self.stopped = True
+# def read(self):
+# # Return the most recent frame
+#     return self.frame
+
+# def stop(self):
+# # Indicate that the camera and thread should be stopped
+#     self.stopped = True
 
 MODEL_NAME = "bumper_detect_m3.tflite"
 GRAPH_NAME = "bumper_detect_m3.tflite"
@@ -87,7 +87,7 @@ else:
 if use_TPU:
     # If user has specified the name of the .tflite file, use that name, otherwise use default 'edgetpu.tflite'
     if (GRAPH_NAME == 'detect.tflite'):
-        GRAPH_NAME = 'edgetpu.tflite'       
+        GRAPH_NAME = 'edgetpu.tflite'
 
 # Get path to current working directory
 
@@ -105,7 +105,7 @@ with open(PATH_TO_LABELS, 'r') as f:
 # If using Edge TPU, use special load_delegate argument
 if use_TPU:
     interpreter = tf.lite.Interpreter(model_path=PATH_TO_CKPT,
-                              experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+                                      experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
     print(PATH_TO_CKPT)
 else:
     interpreter = tf.lite.Interpreter(model_path=PATH_TO_CKPT)
@@ -127,9 +127,9 @@ input_std = 127.5
 # because outputs are ordered differently for TF2 and TF1 models
 outname = output_details[0]['name']
 
-if ('StatefulPartitionedCall' in outname): # This is a TF2 model
+if ('StatefulPartitionedCall' in outname):  # This is a TF2 model
     boxes_idx, classes_idx, scores_idx = 1, 3, 0
-else: # This is a TF1 model
+else:  # This is a TF1 model
     boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
 frame_to_return = None
@@ -139,10 +139,12 @@ frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 t1 = cv2.getTickCount()
 
+
 class boxObj:
     def __init__(self, box, label):
         self.box = box
         self.label = label
+
 
 def run_model(frame):
     global frame_to_return, frame_rate_calc
@@ -163,62 +165,75 @@ def run_model(frame):
         input_data = (np.float32(input_data) - input_mean) / input_std
 
     # Perform the actual detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
 
     # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
-    
+    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[
+        0]  # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[
+        0]  # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[
+        0]  # Confidence of detected objects
+
     box_objs = []
-    
+
     # Loop over all detections and draw detection box if confidence is above minimum threshold
     for i in range(len(scores)):
         if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
 
             # Get bounding box coordinates and draw box
             # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-            ymin = int(max(1,(boxes[i][0] * imH)))
-            xmin = int(max(1,(boxes[i][1] * imW)))
-            ymax = int(min(imH,(boxes[i][2] * imH)))
-            xmax = int(min(imW,(boxes[i][3] * imW)))
-            
-            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
-            
+            ymin = int(max(1, (boxes[i][0] * imH)))
+            xmin = int(max(1, (boxes[i][1] * imW)))
+            ymax = int(min(imH, (boxes[i][2] * imH)))
+            xmax = int(min(imW, (boxes[i][3] * imW)))
+
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+
             # Draw label
-            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
-            
+            # Look up object name from "labels" array using class index
+            object_name = labels[int(classes[i])]
+            label = '%s: %d%%' % (object_name, int(
+                scores[i]*100))  # Example: 'person: 72%'
+            labelSize, baseLine = cv2.getTextSize(
+                label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+            # Make sure not to draw label too close to top of window
+            label_ymin = max(ymin, labelSize[1] + 10)
+            # Draw white box to put label text in
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (
+                xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, label, (xmin, label_ymin-7),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
+
             box_objs += [boxObj([xmin, ymin, xmax, ymax], object_name)]
 
-
     # Draw framerate in corner of frame
-    cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+    cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
 
     frame_to_return = frame
 
     # Calculate framerate
     t2 = cv2.getTickCount()
     time1 = (t2-t1)/freq
-    frame_rate_calc= 1/time1
+    frame_rate_calc = 1/time1
 
     # Press 'q' to quit
     if cv2.waitKey(1) == ord('q'):
         cv2.destroyAllWindows()
     return frame_to_return, box_objs
 
+
 def get_frame():
     return frame_to_return
+
 
 def set_resolution(width, height):
     global imW, imH
     imW = width
     imH = height
-    
+
+
 def get_fps():
     return frame_rate_calc
--- flask_app.py
+++ flask_app.py
@@ -10,26 +10,28 @@ import pandas as pd
 import TFLite_detection_webcam
 import math
 
+
 class Node:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.g = float('inf')  # cost from start node
+        self.g = float('inf')  # cost from start node``
         self.h = float('inf')  # estimated cost to goal node
         self.f = float('inf')  # total cost
         self.parent = None
-        self.obstacle = False 
+        self.obstacle = False
 
     def __lt__(self, other):
         return self.f < other.f
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 
 # Create an empty array to store the clicked points
-obstacles = [[-1,-1]]
-#handles robots
-moving_obstacles = [[1,1]]
+obstacles = [[-1, -1]]
+# handles robots
+moving_obstacles = [[1, 1]]
 
 # Define the size of the grid (decimeeters)
 grid_width = 166
@@ -42,7 +44,7 @@ grid = [[Node(x, y) for y in range(grid_height)] for x in range(grid_width)]
 camera = cv2.VideoCapture("D:/xyzab.mp4")  # use 0 for web camera
 moving_obstacles = []
 
-# decimeters    
+# decimeters
 START = (6, 1)
 GOAL = (164, 80)
 
@@ -51,10 +53,12 @@ MOVE_DIAGONAL_COST = math.sqrt(2)
 
 OUTPUT_FILE = "path_output.path"
 
+
 @app.route('/')
 def index():
-    frame = None #this is a load-bearing line of code
+    frame = None  # this is a load-bearing line of code
     return render_template('index.html', obstacles=obstacles, moving_obstacles=moving_obstacles, new_points=new_points, frame=frame)
+
 
 @app.route('/upload_json', methods=['POST'])
 def upload_json():
@@ -72,6 +76,7 @@ def upload_json():
     else:
         return jsonify({'message': 'No JSON file uploaded.'}), 400
 
+
 @app.route('/add_point', methods=['POST'])
 def add_point():
     x = int(request.form['x'])
@@ -79,36 +84,41 @@ def add_point():
     y = int(request.form['y'])
     clamp(y, 0, grid_height - 1)
     point = [x, y]
-    if [-1,-1] in obstacles:
-        obstacles.remove([-1,-1])
+    if [-1, -1] in obstacles:
+        obstacles.remove([-1, -1])
     if point in obstacles:
         obstacles.remove(point)
         print("Removed point:", point)
-        return json.dumps((-1,-1))
+        return json.dumps((-1, -1))
     else:
         obstacles.append(point)
         print("Added point:", point)
         return json.dumps(point)
+
 
 @app.route('/get_obstacles')
 def get_obstacles():
     obs = obstacles
     return jsonify(obstacles=obs)
 
+
 @app.route('/get_moving_obstacles')
 def get__moving_obstacles():
     movobs = moving_obstacles
     return jsonify(moving_obstacles=movobs)
+
 
 @app.route('/get_fps')
 def get_fps():
     fps = TFLite_detection_webcam.get_fps()
     return jsonify(fps=fps)
 
+
 @app.route('/get_new_obstacles')
 def get_new_obstacles():
     new_points = new_points
     return jsonify(new_points=new_points)
+
 
 @app.route('/clear_obstacles', methods=['POST'])
 def clear_obstacles():
@@ -116,11 +126,12 @@ def clear_obstacles():
     obstacles = []
     return jsonify(message='Obstacles cleared successfully')
 
+
 @app.route('/construct_shape', methods=['POST', 'GET'])
 def construct_shape():
     global shape_points, obstacles
-    if [-1,-1] in obstacles:
-        obstacles.remove([-1,-1])
+    if [-1, -1] in obstacles:
+        obstacles.remove([-1, -1])
     temp = []
     temp = json.loads(request.form['shape_points'])
 
@@ -137,16 +148,17 @@ def construct_shape():
     points += bresenham_line(x4, y4, x1, y1)
     points += bresenham_line(x1, y1, x3, y3)
     points += bresenham_line(x2, y2, x4, y4)
-    
+
     for point in points:
         x, y = point
         x = clamp(x, 0, grid_width - 1)
         y = clamp(y, 0, grid_height - 1)
-        obstacles.append((x,y))
+        obstacles.append((x, y))
     obstacles = pd.Series(obstacles).drop_duplicates().tolist()
     print("New Obstacles:", points)
-    new_points=points
+    new_points = points
     return json.dumps(new_points)
+
 
 def construct_shape(p1, p2, p3, p4):
     x1, y1 = p1
@@ -162,16 +174,17 @@ def construct_shape(p1, p2, p3, p4):
     points += bresenham_line(x4, y4, x1, y1)
     points += bresenham_line(x1, y1, x3, y3)
     points += bresenham_line(x2, y2, x4, y4)
-    
+
     for point in points:
-            x, y = point
-            x = clamp(x, 0, grid_width - 1)
-            y = clamp(y, 0, grid_height - 1)
-            temp.append((x,y))
-    
+        x, y = point
+        x = clamp(x, 0, grid_width - 1)
+        y = clamp(y, 0, grid_height - 1)
+        temp.append((x, y))
+
     temp = pd.Series(temp).drop_duplicates().tolist()
 
     return temp
+
 
 def bresenham_line(x0, y0, x1, y1):
     points = []
@@ -200,14 +213,17 @@ def bresenham_line(x0, y0, x1, y1):
 
     return points
 
+
 def config():
     global grid
     for obstacle in obstacles:
         print(obstacle[0], " , ", obstacle[1])
-        grid[obstacle[0]] [obstacle[1]].obstacle = True
-    
+        grid[obstacle[0]][obstacle[1]].obstacle = True
+
+
 def heuristic(node_a, node_b):
     return abs(node_a.x - node_b.x) + abs(node_a.y - node_b.y)
+
 
 def changed_direction(curr_node, prev_node, prev_prev_node):
     x_delta = curr_node.x - prev_node.x
@@ -218,7 +234,7 @@ def changed_direction(curr_node, prev_node, prev_prev_node):
         return False
     return True
 
-#im gay
+# im gay
 def reverse_linked_list(head):
     prev_node = None
     current_node = head
@@ -232,11 +248,12 @@ def reverse_linked_list(head):
     # Return the new head of the reversed linked list
     return prev_node
 
+
 def generate_path(end_node):
     path = []
     current_node = reverse_linked_list(end_node)
     prev_control = None
-    
+
     prev_node = None
     prev_prev_node = None
     delay = 2
@@ -244,12 +261,12 @@ def generate_path(end_node):
 
     while current_node is not None:
         delay -= 1
-        
+
         anchor_point = {
             "x": current_node.x / 10,
             "y": current_node.y / 10
         }
-        
+
         waypoint = {
             "anchorPoint": anchor_point,
             "prevControl": prev_control,
@@ -269,69 +286,71 @@ def generate_path(end_node):
         if (did_change_dir or current_node.parent is None):
             path.append(waypoint)
         prev_control = anchor_point
-        
+
         prev_prev_node = prev_node
         prev_node = current_node
         current_node = current_node.parent
 
-
         if (current_node is not None and prev_node is not None and prev_prev_node is not None and (delay <= 0)):
-            did_change_dir = changed_direction(current_node, prev_node, prev_prev_node)
+            did_change_dir = changed_direction(
+                current_node, prev_node, prev_prev_node)
     for i in range(len(path)-1):
         path[i]["nextControl"] = path[i+1]["anchorPoint"]
-    
+
     path[0]["prevControl"] = None
-    
+
     path[-1]["nextControl"] = None
-    
+
     return path
+
 
 def astar():
     open_set = PriorityQueue()
-    
+
     start_node = grid[START[0]][START[1]]
     start_node.g = 0
     start_node.h = heuristic(start_node, grid[GOAL[0]][GOAL[1]])
     start_node.f = start_node.g + start_node.h
     open_set.put(start_node)
-    
+
     while not open_set.empty():
         current_node = open_set.get()
-        
+
         if current_node.x == GOAL[0] and current_node.y == GOAL[1]:
             return current_node
-        
+
         neighbors = []
         for dx in [-1, 0, 1]:
             for dy in [-1, 0, 1]:
                 if dx == 0 and dy == 0:
                     continue
-                
+
                 new_x = current_node.x + dx
                 new_y = current_node.y + dy
-                
+
                 if 0 <= new_x < grid_width and 0 <= new_y < grid_height:
                     neighbors.append(grid[new_x][new_y])
-        
+
         for neighbor in neighbors:
             if neighbor.obstacle:
                 continue
-            
+
             if abs(neighbor.x - current_node.x) + abs(neighbor.y - current_node.y) == 1:
                 tentative_g_score = current_node.g + MOVE_STRAIGHT_COST
             else:
                 tentative_g_score = current_node.g + MOVE_DIAGONAL_COST
-            
+
             if tentative_g_score < neighbor.g:
                 neighbor.parent = current_node
                 neighbor.g = tentative_g_score
                 neighbor.h = heuristic(neighbor, grid[GOAL[0]][GOAL[1]])
                 neighbor.f = neighbor.g + neighbor.h
-                
+
                 if neighbor not in open_set.queue:
                     open_set.put(neighbor)
-    
+
     return None
+
 
 @app.route('/runny_nose', methods=['POST', 'GET'])
 def run():
@@ -348,18 +367,20 @@ def run():
         output_data["waypoints"] = generate_path(path)
     else:
         print("No path found")
-    
+
     with open("path_output.path", 'w') as file:
         json.dump(output_data, file, indent=2)
-    
+
     file_path = 'path_output.path'
 
     return send_file(file_path, as_attachment=True)
 
-def clamp(num, min_value, max_value):
-   return max(min(num, max_value), min_value)
 
-def gen_frames(): 
+def clamp(num, min_value, max_value):
+    return max(min(num, max_value), min_value)
+
+
+def gen_frames():
     global moving_obstacles
     while True:
         success, frame = camera.read()
@@ -368,7 +389,7 @@ def gen_frames():
         else:
             frame, coords = TFLite_detection_webcam.run_model(frame)
             for obs in moving_obstacles:
-                grid[obs[0]] [obs[1]].obstacle = False
+                grid[obs[0]][obs[1]].obstacle = False
             print(moving_obstacles)
             moving_obstacles = []
             ret, buffer = cv2.imencode('.jpg', frame)
@@ -378,33 +399,38 @@ def gen_frames():
                 y1 = obj.box[1]
                 x2 = obj.box[2]
                 y2 = obj.box[3]
-                x_1, y_1, x_2, y_2 = define_world_space_position(x1, y1, x2, y2)
+                x_1, y_1, x_2, y_2 = define_world_space_position(
+                    x1, y1, x2, y2)
                 x_1 = int(x_1)
                 x_2 = int(x_2)
                 y_1 = int(y_1)
                 y_2 = int(y_2)
-                moving_obstacles += construct_shape((x_1, y_1), (x_2, y_2), (x_1, y_2), (x_2, y_1))
+                moving_obstacles += construct_shape(
+                    (x_1, y_1), (x_2, y_2), (x_1, y_2), (x_2, y_1))
                 for obs in moving_obstacles:
-                    grid[obs[0]] [obs[1]].obstacle = True
+                    grid[obs[0]][obs[1]].obstacle = True
 
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
-            
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
 def define_world_space_position(x1, y1, x2, y2):
     global camera, fov_horizontal, fov_vertical
     # Camera specifications
-    resolution_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))  # Width resolution of the camera
-    resolution_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Height resolution of the camera
+    # Width resolution of the camera
+    resolution_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # Height resolution of the camera
+    resolution_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fov_horizontal = 60  # Horizontal field of view in degrees
     fov_vertical = 40  # Vertical field of view in degrees
 
     # Real-world dimensions
-    grid_width = 16.5  
-    grid_height = 8  
+    grid_width = 16.5
+    grid_height = 8
 
     # Camera position on the grid
-    camera_x = 82  
-    camera_y = 40  
+    camera_x = 82
+    camera_y = 40
 
     # Camera rotation angle in degrees
     camera_rotation = 45  # Example angle
@@ -424,20 +450,28 @@ def define_world_space_position(x1, y1, x2, y2):
     box_center_y = (box_ymin + box_ymax) / 2
 
     # Calculate the angle offsets from the center of the camera's FOV
-    angle_offset_horizontal = (box_center_x - resolution_width / 2) * pixel_size_horizontal
-    angle_offset_vertical = (box_center_y - resolution_height / 2) * pixel_size_vertical
+    angle_offset_horizontal = (
+        box_center_x - resolution_width / 2) * pixel_size_horizontal
+    angle_offset_vertical = (
+        box_center_y - resolution_height / 2) * pixel_size_vertical
 
     # Convert the angle offsets to real-world distances
-    distance_horizontal = math.tan(math.radians(angle_offset_horizontal)) * grid_width / 2
-    distance_vertical = math.tan(math.radians(angle_offset_vertical)) * grid_height / 2
+    distance_horizontal = math.tan(math.radians(
+        angle_offset_horizontal)) * grid_width / 2
+    distance_vertical = math.tan(math.radians(
+        angle_offset_vertical)) * grid_height / 2
 
     # Adjust the angle offsets based on the camera's rotation angle
-    adjusted_angle_offset_horizontal = math.radians(camera_rotation) + math.radians(angle_offset_horizontal)
-    adjusted_angle_offset_vertical = math.radians(camera_rotation) + math.radians(angle_offset_vertical)
+    adjusted_angle_offset_horizontal = math.radians(
+        camera_rotation) + math.radians(angle_offset_horizontal)
+    adjusted_angle_offset_vertical = math.radians(
+        camera_rotation) + math.radians(angle_offset_vertical)
 
     # Convert the adjusted angle offsets to real-world distances
-    adjusted_distance_horizontal = math.tan(adjusted_angle_offset_horizontal) * grid_width / 2
-    adjusted_distance_vertical = math.tan(adjusted_angle_offset_vertical) * grid_height / 2
+    adjusted_distance_horizontal = math.tan(
+        adjusted_angle_offset_horizontal) * grid_width / 2
+    adjusted_distance_vertical = math.tan(
+        adjusted_angle_offset_vertical) * grid_height / 2
 
     # Calculate the box size in real-world space
     box_width_pixels = box_xmax - box_xmin
@@ -446,23 +480,28 @@ def define_world_space_position(x1, y1, x2, y2):
     box_height_real = box_height_pixels * pixel_size_vertical
 
     # Calculate the coordinates of the box's corners in real-world space
-    bottom_left_x = camera_x + distance_horizontal - (box_width_real / 2) + adjusted_distance_horizontal
-    bottom_left_y = camera_y + distance_vertical - (box_height_real / 2) + adjusted_distance_vertical
+    bottom_left_x = camera_x + distance_horizontal - \
+        (box_width_real / 2) + adjusted_distance_horizontal
+    bottom_left_y = camera_y + distance_vertical - \
+        (box_height_real / 2) + adjusted_distance_vertical
     top_right_x = bottom_left_x + box_width_real
     top_right_y = bottom_left_y + box_height_real
 
     return (bottom_left_x, bottom_left_y, top_right_x, top_right_y)
+
 
 def set_fov(fov_h, fov_v):
     global fov_horizontal, fov_vertical
     fov_horizontal = fov_h
     fov_vertical = fov_v
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-            
-if __name__ == '__main__':
-    square_size = min(math.floor(800 / grid_height), math.floor(1650 / grid_width))
-    app.run()
 
+
+if __name__ == '__main__':
+    square_size = min(math.floor(800 / grid_height),
+                      math.floor(1650 / grid_width))
+    app.run()
